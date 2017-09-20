529756a47c102607832c0e6fe5e41f0b991c0290
Kirill Nosenko <mensork@yandex.ru>
2017-09-20 17:03:56 +0300

    badges of cards

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,6 +1,15 @@
+if (Gem.win_platform?)
+  Encoding.default_external = Encoding.find(Encoding.locale_charmap)
+  Encoding.default_internal = __ENCODING__
+
+  [STDIN, STDOUT].each do |io|
+    io.set_encoding(Encoding.default_external, Encoding.default_internal)
+  end
+end
+
 # Массивы с наборами мастей и достоинств
 values = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
-suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades'] # Буби, Черви, Крести, Пики
+suits = ['♦', '♥', '♣', '♠'] # Буби, Черви, Крести, Пики
 
 # Выведем произвольную карты, выбрав по одному элементу из массивов
-puts "#{values.sample} of #{suits.sample}"
+puts "#{values.sample}#{suits.sample}"
